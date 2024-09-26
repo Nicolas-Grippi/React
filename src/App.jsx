f741fbdb313d643baaaf9ccb72b7a8da6854bc11
nicolas grippi <nicolasgrippi96gmail.com>
2024-09-26 20:05:59 -0300

    Agregando routing y nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer'
+//import ItemListContainer from './components/ItemListContainer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomeView from './views/HomeView/HomeView'
+import ProductView from './views/ProductView/ProductView'
+import ContactView from './views/ContactView/ContactView'
 
 function App() {
   return (
     <>
-      <NavBar />
-      <ItemListContainer greetings="Bienvenidos a tienda Scaloneta" />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+
+       <Route exact path="/" element={<HomeView />}/>
+       <Route exact path="/products" element={<ProductView />}/> 
+       <Route exact path="/contact" element={<ContactView />}/>
+
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,20 +2,22 @@
 import React from 'react';
 import './NavBar.css';
 import CartWidget from './CartWidget';
+import {Link} from 'react-router-dom';
 
 export default function NavBar() {
   return (
+  <>
     <nav className="my-4 d-flex justify-content-between align-items-center">
       <img className='img-fluid' src="https://seeklogo.com/images/A/afa-argentina-3-star-logo-CE85FC39D9-seeklogo.com.png" alt="marca" />
       <div>    
-     <button className='btn btn-dark mx-2'>Productos</button>
-     <button className='btn btn-dark mx-2'>Outlet</button>
-     <button className='btn btn-dark mx-2'>Nosotros</button>
-     <button className='btn btn-dark mx-2'>Contacto</button>
+      <button><Link to={'/'} className='btn btn-dark mx-2'>Home</Link></button>
+     <button><Link to={'/'} className='btn btn-dark mx-2'>Camisetas</Link></button>
+     <button><Link to={'/'} className='btn btn-dark mx-2'>Pantalones</Link></button>
+     <button><Link to={'/'} className='btn btn-dark mx-2'>Contacto</Link></button>
      </div>
       <CartWidget />
-      
     </nav>
+    </>
   );
 }
 
