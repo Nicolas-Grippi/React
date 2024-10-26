52b876129a5025b6a0141a8394373a9d1950c062
nicolas grippi <nicolasgrippi96gmail.com>
2024-10-25 21:13:31 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from './components/NavBar';
 import Footer from './components/Footer';
 import Cart from './components/Cart';
 import ItemListContainer from './components/ItemListContainer';
-import ItemDetailsContainer from './components/ItemDetailsContainer';
+import itemDetailsContainer from './components/itemDetailsContainer';
 import CheckOut from './components/Checkout';
 import { CartProvider } from "../context/CartContext";
 
@@ -22,7 +22,7 @@ function App() {
               <Route exact path="/" element={<HomeView />} />
               <Route exact path="/infobuyer" element={<InfoView />} />
               <Route path="/category/:id" element={<ItemListContainer />} />
-              <Route path="/item/:id" element={<ItemDetailsContainer />} />
+              <Route path="/item/:id" element={<itemDetailsContainer />} />
               <Route exact path="/cart" element={<Cart />} />
               <Route path="/checkout/:orderId" element={<CheckOut />} />
             </Routes>
